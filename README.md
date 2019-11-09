ea737c0ed9e7dca61839fdb197296109b9207870
Eric <57112072+ekfitz@users.noreply.github.com>
2019-11-08 20:31:06 -0700

    Junk this is horrible

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 Menu in top-right. On click, expands down revealing navigation text.
 Transitions to enhance UX and responsive window re-size with JS.
+
+
+This is junk and I'm going to start fresh
--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-var nb1 = {t: "RECENT", l: "0%", d: "nb1"};
-var nb2 = {t: "ABOUT", l: "33%", d: "nb2"};
-var nb3 = {t: "CONTACT", l: "66%", d:"nb3"};
+var nb1 = {t: "RECENT", l: "0%", d: "naba1"};
+var nb2 = {t: "ABOUT", l: "33%", d: "naba2"};
+var nb3 = {t: "CONTACT", l: "66%", d:"naba3"};
 
 setNav();
 setBox(nb1.d, nb1.l);
 setBox(nb2.d, nb2.l);
 setBox(nb3.d, nb3.l);
+
+var naba1 = document.getElementById("naba1");
+var naba2 = document.getElementById("naba2");
+var naba3 = document.getElementById("naba3");
+
+setText(nb1.t, "naba1");
 
 function setNav() {
   var nav = document.createElement("div");
@@ -21,4 +27,14 @@ function setBox(x, y) {
   box.id = x;
   box.style.left = y;
   navbox.appendChild(box);
+}
+
+// x = nb1.t, y = var id;
+function setText(x, y) {
+  var tst = document.getElementById(y);
+  var ts = document.createElement("span");
+  var text = document.createTextNode(x);
+  ts.appendChild(text);
+  ts.className = "navtext";
+  y.appendChild(text);
 }
